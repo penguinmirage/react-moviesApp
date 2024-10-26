2d19cd1920f106dd1e68bc8fe591f83a08501fb3
Petr Chukhnov <peka4u@vk.com>
2024-10-27 01:44:48 +0300

    New commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { format } from 'date-fns'; 
 
 import FilterComponent from '../filter-component';
 import SearchComponent from '../search-component';
 import MovieCardsList from '../movie-cards-list';
 import MovieapiService from '../../api-service/movieapi.js';
 import './app.css';
+
+// Function to truncate text
+function truncateText(text, maxLength) {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  const truncated = text.slice(0, maxLength);
+  return truncated.slice(0, truncated.lastIndexOf(' ')) + '...';
+}
+
+// Function to format date
+function formatDate(releaseDate) {
+  if (!releaseDate) {
+    return 'Release date not available';
+  }
+
+  const date = new Date(releaseDate);
+  if (isNaN(date)) {
+    return 'Release date not available';
+  }
+
+  return format(date, 'MMMM d, yyyy'); // Format date as "Month Day, Year"
+}
 
 export default class App extends Component {
   // Define movieapi as a class property
@@ -39,8 +63,8 @@ export default class App extends Component {
             {movies.map(movie => (
               <li key={movie.id}>
                 <h2>{movie.title}</h2>
-                <p>Release Date: {movie.release_date}</p>
-                <p>{movie.overview}</p>
+                <p>Release Date: {formatDate(movie.release_date)}</p> 
+                <p>{truncateText(movie.overview, 100)}</p> 
               </li>
             ))}
           </ul>
